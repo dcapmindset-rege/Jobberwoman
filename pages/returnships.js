d8ea12315c9ea73941bf82fd5295382d67c9e19e
dcapmindset-rege <dcapmindset@gmail.com>
2026-03-31 15:27:18 +0100

    Update returnships.js

--- pages/returnships.js
+++ pages/returnships.js
@@ -1,1 +1,16 @@
+import Layout from '../components/Layout';
+import JobCard from '../components/JobCard';
+import { returnships } from '../data/returnships';
 
+export default function Returnships() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold mb-6">Returnship Programs</h1>
+      <div className="grid md:grid-cols-2 gap-6">
+        {returnships.map(program => (
+          <JobCard key={program.id} job={program} type="returnship" />
+        ))}
+      </div>
+    </Layout>
+  );
+}
