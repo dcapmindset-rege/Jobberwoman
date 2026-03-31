008c52f4fbcef93c876e040d7e84d419ba682bf2
dcapmindset-rege <dcapmindset@gmail.com>
2026-03-31 15:26:36 +0100

    Update jobs.js

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,1 +1,16 @@
+import Layout from '../components/Layout';
+import JobCard from '../components/JobCard';
+import { jobs } from '../data/jobs';
 
+export default function Jobs() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold mb-6">Job Opportunities</h1>
+      <div className="grid md:grid-cols-2 gap-6">
+        {jobs.map(job => (
+          <JobCard key={job.id} job={job} type="job" />
+        ))}
+      </div>
+    </Layout>
+  );
+}
